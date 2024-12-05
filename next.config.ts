950713c78f7d507abe6a1c5a86406f0b698e5484
AndresRamos <andresydana611@gmail.com>
2024-12-05 17:29:21 -0500

    carga de imagenes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: 'dist',
+    images: {
+        unoptimized: true,  // Desactivar la optimización de imágenes
+    },
 };
 
 export default nextConfig;
