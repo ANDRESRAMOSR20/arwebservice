0b87e03e5fde25b8e0c3b4c9181dd56d4bc9f46b
AndresRamos <andresydana611@gmail.com>
2024-12-14 08:59:49 -0500

    push beginnig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV !== "production";
 const nextConfig: NextConfig = {
+    basePath: isProduction ? "arwebservice.com" : "",
     output: 'export',
     distDir: 'dist',
     images: {
